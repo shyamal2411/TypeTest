be98a15956af7cb8b44383b5af5c351c22c45989
Shyamal <sgprajapati24@gmail.com>
2021-05-04 00:28:06 +0530

    efforts in vein

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,13 +1,28 @@
 import React from "react";
 import "./Main.css";
-
+import flash from '../../assets/hero.png'
 const Main = () => {
     return (
-        <div className="Main-container">
-        <div className="landing-left">
+        <div className="main-container">
+        <div  data-aos="fade-right" className="landing-left">
             <h1 className="landing-header">
                 Can you type?
             </h1>
+            <div className="typeriter-container">
+                <h1>Fast</h1>
+                <h1>Correct?</h1>
+                <h1>Quick?</h1>
+            </div>
+            <div 
+            data-aos="fade-left"
+             className="main-right">
+                <img 
+                src={flash} 
+                className="hero-image" 
+                alt="hero"
+
+                />
+            </div>
         </div>
         </div>
     );
