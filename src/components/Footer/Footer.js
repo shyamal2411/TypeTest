59d6a9861e7bc579209eb28ed9d6ef40abf7d70c
Shyamal <sgprajapati24@gmail.com>
2021-05-06 23:49:03 +0530

    Footer Functional
    Note: Need to change color

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+    return (
+        <div className="footer-container">
+            <a
+                href="https://www.linkedin.com/in/sgprajapati/"
+                className="footer-link"
+                target="_blank"
+                rel="noreferrer">Let's Connect!</a>
+        </div>
+    );
+};
+
+export default Footer;
