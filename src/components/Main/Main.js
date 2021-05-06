01e38f78eb5712ccdd455696144898d8f19194b6
Shyamal <sgprajapati24@gmail.com>
2021-05-06 23:50:03 +0530

    Page working
    Fixed issues of typewriting effect

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,29 +1,31 @@
 import React from "react";
 import "./Main.css";
-import flash from '../../assets/hero.png'
+import flash from "./../../assets/hero.png";
+// import Typewriter from "typewriter-effect";
+import Typical from 'react-typical'
+
 const Main = () => {
     return (
-        <div className="main-container">
-        <div  data-aos="fade-right" className="landing-left">
-            <h1 className="landing-header">
-                Can you type?
-            </h1>
-            <div className="typeriter-container">
-                <h1>Fast</h1>
-                <h1>Correct?</h1>
-                <h1>Quick?</h1>
+        <div className="landing-container">
+            <div data-aos="fade-right" className="landing-left">
+                <h1 className="landing-header">Can you type...</h1>
+                <div className="typewriter-container">
+                 <Typical
+                  steps={['Correct?', 1000, 'Fast!', 500, 'Quick?',1000]}
+                  loop={Infinity}
+                  wrapper="p"
+                    />
+                </div>
             </div>
-            <div 
-            data-aos="fade-left"
-             className="main-right">
-                <img 
-                src={flash} 
-                className="hero-image" 
-                alt="hero"
-
+            
+            <div className="landing-right">
+                <img
+                    data-aos="fade-left"
+                    className="flash-image"
+                    src={flash}
+                    alt="hero"
                 />
             </div>
-        </div>
         </div>
     );
 };
