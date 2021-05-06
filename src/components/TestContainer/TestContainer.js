4e6269f0aaaaa199f16a5a66be7181a3353e5a14
Shyamal <sgprajapati24@gmail.com>
2021-05-06 23:51:00 +0530

    Testing area

---
+++ src/components/TestContainer/TestContainer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import TryAgain from '../TryAgain/TryAgain';
+import "./TestContainer.css"
+const TestContainer = ({
+    words,
+    characters, 
+    wpm
+}) => {
+    return(
+<div className="test-container">
+    <div className="try-again">
+        <TryAgain words={words} characters={characters} wpm={wpm} />
+    </div>
+</div>
+    );
+}
+
+export default TestContainer;
