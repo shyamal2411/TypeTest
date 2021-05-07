4addbba8e5eedeabc80b98dfee6eb693c04fefd4
Shyamal <sgprajapati24@gmail.com>
2021-05-08 00:30:48 +0530

    Msg not displayed on scrn
    upto 6:13

---
+++ src/components/TypingChallenge/TypingChallenge.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./TypingChallenge.css";
+
+const TypingChallenge = (selectedParagraph) => {
+  return (
+    <div className="typing-challenge">
+      <div className="timer-container">
+        <p className="timer">00:60</p>
+        <p className="timer-info">START TYPING</p>
+      </div>
+      <div className="textarea-main">
+        <div className="textarea-left">
+           <div className="textarea test-paragraph">
+            {selectedParagraph}
+           </div>
+            </div>
+
+        <div className="textarea-right">
+        <textarea 
+        className="textarea"
+        placeholder="Start Typing Here!"
+        ></textarea>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TypingChallenge;
