e67cdd5ffae1aeb5c5c7fe9c9c0fc40886d001fd
Shyamal <sgprajapati24@gmail.com>
2021-05-07 15:52:34 +0530

    Added Challenge Container

--- src/components/TestContainer/TestContainer.js
+++ src/components/TestContainer/TestContainer.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import TryAgain from '../TryAgain/TryAgain';
-import "./TestContainer.css"
-const TestContainer = ({
-    words,
-    characters, 
-    wpm
-}) => {
-    return(
-<div className="test-container">
-    <div className="try-again">
+import React from "react";
+import TryAgain from "../TryAgain/TryAgain";
+import TypingChallengeContainer from "../TypingContainer/TypingContainer";
+import "./TestContainer.css";
+const TestContainer = ({ words, characters, wpm }) => {
+  return (
+    <div className="test-container">
+      <div className="typing-challenge-container" data-aos="fade-up">
+        <TypingChallengeContainer
+          words={words}
+          characters={characters}
+          wpm={wpm}
+        />
+      </div>
+      {/* <div className="try-again">
         <TryAgain words={words} characters={characters} wpm={wpm} />
+    </div> */}
     </div>
-</div>
-    );
-}
+  );
+};
 
 export default TestContainer;
