1efe447b27100d6d27c8da97fb0e51486cf47d8a
Shyamal Prajapati <sgprajapati24@gmail.com>
2021-05-10 22:25:46 +0530

    appjs

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,18 +6,35 @@ import Main from "../Main/Main";
 import Navbar from "../Navbar/Navbar";
 import "./App.css";
 
+const TotalTime = 60;
+
 class App extends React.Component {
+  state = {
+    selectedParagraph: "hey SG!",
+    timerStarted: false,
+    timeRemaining: TotalTime,
+    words: 0,
+    characters: 0,
+    wpm: 0
+  };
   render() {
     return (
       <div className="app">
         {/* Nav section */}
-      <Navbar/>
+        <Navbar />
         {/* Landing section */}
-      <Main/>
+        <Main />
         {/* Challenge section */}
-      <Challenge/>
+        <Challenge
+          selectedParagraph={this.state.selectedParagraph}
+          words={this.state.words}
+          characters={this.state.characters}
+          wpm={this.state.wpm}
+          timeRemaining={this.state.timeRemaining}
+          timerStarted={this.state.timerStarted}
+        />
         {/* Footer */}
-        <Footer/>
+        <Footer />
       </div>
     );
   }
