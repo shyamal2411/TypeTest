ea9dd6f2417f6c411720eddbdfa774a2cd7056e4
Shyamal Prajapati <sgprajapati24@gmail.com>
2021-05-10 23:20:51 +0530

    chellengeSectionjs

--- src/components/Challenge/ChallengeSection.js
+++ src/components/Challenge/ChallengeSection.js
@@ -1,15 +1,28 @@
 import React from "react";
 import TestContainer from "../TestContainer/TestContainer";
-import "./ChallengeSection.css"
-const Challenge = ()=>{
-    return (
-        <div className="challenge-container">
-            <h1 data-aos="fade-down" className="challenge-section-header">
-                Take a speed test Now!
-            </h1>
-            <TestContainer words={4} characters={14} wpm={20} />
-        </div>
-    );
-}
+import "./ChallengeSection.css";
+const Challenge = ({
+  selectedParagraph,
+  words,
+  characters,
+  wpm,
+  timeRemaining,
+  timerStarted
+}) => {
+  return (
+    <div className="challenge-container">
+      <h1 data-aos="fade-down" className="challenge-section-header">
+        Take a speed test Now!
+      </h1>
+      <TestContainer
+        timeRemaining={timeRemaining}
+        words={words}
+        characters={characters}
+        wpm={wpm}
+        timerStarted={timerStarted}
+      />
+    </div>
+  );
+};
 
 export default Challenge;
