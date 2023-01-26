d13155c9bae5296602c3cb395a6c8126d99e61be
SG <sgprajapati24@gmail.com>
2023-01-26 20:02:04 +0530

    bug fixed
    letter-spacing was too small

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,188 +1,193 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { SAMPLE_PARAGRAPHS } from "../../data/sampleParagraphs";
 import ChallengeSection from "../ChallengeSection/ChallengeSection";
 import Footer from "../Footer/Footer";
 import Main from "../Main/Main";
 import Navbar from "../Navbar/Navbar";
-import { SAMPLE_PARAGRAPHS } from "../../data/sampleParagraphs";
 import "./App.css";
 
 const TotalTime = 60;
 const DefaultState = {
-  selectedParagraph: "Hello World!",
-  testInfo: [],
-  timerStarted: false,
-  timeRemaining: TotalTime,
-  words: 0,
-  characters: 0,
-  wpm: 0
+    selectedParagraph: "Hello World!",
+    testInfo: [],
+    timerStarted: false,
+    timeRemaining: TotalTime,
+    words: 0,
+    characters: 0,
+    wpm: 0,
 };
 
 class App extends React.Component {
-  // state = {
-  //     selectedParagraph: "Hello World!",
-  //     testInfo: [],
-  //     timerStarted: false,
-  //     timeRemaining: TotalTime,
-  //     words: 0,
-  //     characters: 0,
-  //     wpm: 0,
-  // };
-  state = DefaultState;
+    // state = {
+    //     selectedParagraph: "Hello World!",
+    //     testInfo: [],
+    //     timerStarted: false,
+    //     timeRemaining: TotalTime,
+    //     words: 0,
+    //     characters: 0,
+    //     wpm: 0,
+    // };
+    state = DefaultState;
 
-  fetchNewParagraphFallback = () => {
-    const data =
-      SAMPLE_PARAGRAPHS[Math.floor(Math.random() * SAMPLE_PARAGRAPHS.length)];
+    fetchNewParagraphFallback = () => {
+        const data =
+            SAMPLE_PARAGRAPHS[
+                Math.floor(Math.random() * SAMPLE_PARAGRAPHS.length)
+            ];
 
-    const selectedParagraphArray = data.split("");
-    const testInfo = selectedParagraphArray.map((selectedLetter) => {
-      return {
-        testLetter: selectedLetter,
-        status: "notAttempted"
-      };
-    });
-
-    // Update the testInfo in state
-    this.setState({
-      ...DefaultState,
-      selectedParagraph: data,
-      testInfo
-    });
-  };
-
-  fetchNewParagraph = () => {
-    fetch("http://metaphorpsum.com/paragraphs/1/9")
-      .then((response) => response.text())
-      .then((data) => {
-        // Once the api results are here, break the selectedParagraph into test info
         const selectedParagraphArray = data.split("");
         const testInfo = selectedParagraphArray.map((selectedLetter) => {
-          return {
-            testLetter: selectedLetter,
-            status: "notAttempted"
-          };
+            return {
+                testLetter: selectedLetter,
+                status: "notAttempted",
+            };
         });
 
         // Update the testInfo in state
         this.setState({
-          ...DefaultState,
-          selectedParagraph: data,
-          testInfo
+            ...DefaultState,
+            selectedParagraph: data,
+            testInfo,
         });
-      });
-  };
+    };
 
-  componentDidMount() {
-    // As soon as the component mounts, load the selected paragraph from the API
-    this.fetchNewParagraphFallback();
-  }
+    fetchNewParagraph = () => {
+        fetch("http://metaphorpsum.com/paragraphs/1/9")
+            .then((response) => response.text())
+            .then((data) => {
+                // Once the api results are here, break the selectedParagraph into test info
+                const selectedParagraphArray = data.split("");
+                const testInfo = selectedParagraphArray.map(
+                    (selectedLetter) => {
+                        return {
+                            testLetter: selectedLetter,
+                            status: "notAttempted",
+                        };
+                    }
+                );
 
-  startAgain = () => this.fetchNewParagraphFallback();
+                // Update the testInfo in state
+                this.setState({
+                    ...DefaultState,
+                    selectedParagraph: data,
+                    testInfo,
+                });
+            });
+    };
 
-  startTimer = () => {
-    this.setState({ timerStarted: true });
-    const timer = setInterval(() => {
-      if (this.state.timeRemaining > 0) {
-        // Change the WPM and Time Remaining
-        const timeSpent = TotalTime - this.state.timeRemaining;
-        const wpm =
-          timeSpent > 0 ? (this.state.words / timeSpent) * TotalTime : 0;
+    componentDidMount() {
+        // As soon as the component mounts, load the selected paragraph from the API
+        this.fetchNewParagraphFallback();
+    }
+
+    startAgain = () => this.fetchNewParagraphFallback();
+
+    startTimer = () => {
+        this.setState({ timerStarted: true });
+        const timer = setInterval(() => {
+            if (this.state.timeRemaining > 0) {
+                // Change the WPM and Time Remaining
+                const timeSpent = TotalTime - this.state.timeRemaining;
+                const wpm =
+                    timeSpent > 0
+                        ? (this.state.words / timeSpent) * TotalTime
+                        : 0;
+                this.setState({
+                    timeRemaining: this.state.timeRemaining - 1,
+                    wpm: parseInt(wpm),
+                });
+            } else {
+                clearInterval(timer);
+            }
+        }, 1000);
+    };
+
+    handleUserInput = (inputValue) => {
+        if (!this.state.timerStarted) this.startTimer();
+
+        /**
+         * 1. Handle the underflow case - all characters should be shown as not-attempted
+         * 2. Handle the overflow case - early exit
+         * 3. Handle the backspace case
+         *      - Mark the [index+1] element as notAttempted
+         *        (irrespective of whether the index is less than zero)
+         *      - But, don't forget to check for the overflow here
+         *        (index + 1 -> out of bound, when index === length-1)
+         * 4. Update the status in test info
+         *      1. Find out the last character in the inputValue and it's index
+         *      2. Check if the character at same index in testInfo (state) matches
+         *      3. Yes -> Correct
+         *         No  -> Incorrect (Mistake++)
+         * 5. Irrespective of the case, characters, words and wpm can be updated
+         */
+
+        const characters = inputValue.length;
+        const words = inputValue.split(" ").length;
+        const index = characters - 1;
+
+        if (index < 0) {
+            this.setState({
+                testInfo: [
+                    {
+                        testLetter: this.state.testInfo[0].testLetter,
+                        status: "notAttempted",
+                    },
+                    ...this.state.testInfo.slice(1),
+                ],
+                characters,
+                words,
+            });
+
+            return;
+        }
+
+        if (index >= this.state.selectedParagraph.length) {
+            this.setState({
+                characters,
+                words,
+            });
+            return;
+        }
+
+        // Make a copy
+        const testInfo = this.state.testInfo;
+        if (!(index === this.state.selectedParagraph.length - 1))
+            testInfo[index + 1].status = "notAttempted";
+
+        // Check for mistake
+        const isMistake = inputValue[index] === testInfo[index].testLetter;
+
+        // Update the testInfo
+        testInfo[index].status = isMistake ? "correct" : "incorrect";
+
+        // Update the state
         this.setState({
-          timeRemaining: this.state.timeRemaining - 1,
-          wpm: parseInt(wpm)
+            testInfo,
+            words,
+            characters,
         });
-      } else {
-        clearInterval(timer);
-      }
-    }, 1000);
-  };
+    };
 
-  handleUserInput = (inputValue) => {
-    if (!this.state.timerStarted) this.startTimer();
-
-    /**
-     * 1. Handle the underflow case - all characters should be shown as not-attempted
-     * 2. Handle the overflow case - early exit
-     * 3. Handle the backspace case
-     *      - Mark the [index+1] element as notAttempted
-     *        (irrespective of whether the index is less than zero)
-     *      - But, don't forget to check for the overflow here
-     *        (index + 1 -> out of bound, when index === length-1)
-     * 4. Update the status in test info
-     *      1. Find out the last character in the inputValue and it's index
-     *      2. Check if the character at same index in testInfo (state) matches
-     *      3. Yes -> Correct
-     *         No  -> Incorrect (Mistake++)
-     * 5. Irrespective of the case, characters, words and wpm can be updated
-     */
-
-    const characters = inputValue.length;
-    const words = inputValue.split(" ").length;
-    const index = characters - 1;
-
-    if (index < 0) {
-      this.setState({
-        testInfo: [
-          {
-            testLetter: this.state.testInfo[0].testLetter,
-            status: "notAttempted"
-          },
-          ...this.state.testInfo.slice(1)
-        ],
-        characters,
-        words
-      });
-
-      return;
+    render() {
+        return (
+            <div className="app">
+                <Navbar />
+                <Main />
+                <ChallengeSection
+                    selectedParagraph={this.state.selectedParagraph}
+                    testInfo={this.state.testInfo}
+                    onInputChange={this.handleUserInput}
+                    words={this.state.words}
+                    characters={this.state.characters}
+                    wpm={this.state.wpm}
+                    timeRemaining={this.state.timeRemaining}
+                    timerStarted={this.state.timerStarted}
+                    startAgain={this.startAgain}
+                />
+                <Footer />
+            </div>
+        );
     }
-
-    if (index >= this.state.selectedParagraph.length) {
-      this.setState({
-        characters,
-        words
-      });
-      return;
-    }
-
-    // Make a copy
-    const testInfo = this.state.testInfo;
-    if (!(index === this.state.selectedParagraph.length - 1))
-      testInfo[index + 1].status = "notAttempted";
-
-    // Check for mistake
-    const isMistake = inputValue[index] === testInfo[index].testLetter;
-
-    // Update the testInfo
-    testInfo[index].status = isMistake ? "correct" : "incorrect";
-
-    // Update the state
-    this.setState({
-      testInfo,
-      words,
-      characters
-    });
-  };
-
-  render() {
-    return (
-      <div className="app">
-        <Navbar />
-        <Main />
-        <ChallengeSection
-          selectedParagraph={this.state.selectedParagraph}
-          testInfo={this.state.testInfo}
-          onInputChange={this.handleUserInput}
-          words={this.state.words}
-          characters={this.state.characters}
-          wpm={this.state.wpm}
-          timeRemaining={this.state.timeRemaining}
-          timerStarted={this.state.timerStarted}
-          startAgain={this.startAgain}
-        />
-        <Footer />
-      </div>
-    );
-  }
 }
 
 export default App;
